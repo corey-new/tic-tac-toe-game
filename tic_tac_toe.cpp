e62418dd1d1b2488b5c6709c00e7c300ffaaf1dd
Vong Chen <43527143+kearkyle@users.noreply.github.com>
2022-04-30 12:56:07 -0700

    Add function to update board on player move

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -6,37 +6,6 @@
 
 using namespace std;
 
-
-class boardOptions {
-    public:
-    // char array to create board space holder
-    // c_str() to convert to 'X' or 'O' character
-    vector <int>game_board;
-
-    boardOptions() {
-
-        for(int i = 0; i < 10; i++) {
-            game_board.push_back(i);        
-        }
-    }
-
-        void newBoard() {
-
-            cout << "   |   |  " << endl;
-            cout << " " << game_board[0] << " | " << game_board[1] << " | " << game_board[2] << endl;
-            cout << "___|___|___" << endl;
-            cout << " " << game_board[3] << " | " << game_board[4] << " | " << game_board[5] << endl;
-            cout << "___|___|___" << endl;
-            
-            cout << " " << game_board[6] << " | " << game_board[7] << " | " << game_board[8] << endl;
-            cout << "   |   |  " << endl;
-
-            cout << endl;
-
-        }
-
-}; 
-
 class playerOptions {
     public:
     // firstPlayer will always have the first move
@@ -65,11 +34,42 @@ class playerOptions {
 
 };
 
+class boardOptions {
+    public:
+    // char array to create board space holder
+    // c_str() to convert to 'X' or 'O' character
+    vector <string>game_board;
 
+    boardOptions() {
 
+        for(int i = 0; i < 10; i++) {
+            game_board.push_back(std::to_string(i));        
+        }
+    }
 
+        void newBoard() {
 
+            cout << "   |   |  " << endl;
+            cout << " " << game_board[0] << " | " << game_board[1] << " | " << game_board[2] << endl;
+            cout << "___|___|___" << endl;
+            cout << " " << game_board[3] << " | " << game_board[4] << " | " << game_board[5] << endl;
+            cout << "___|___|___" << endl;
+            
+            cout << " " << game_board[6] << " | " << game_board[7] << " | " << game_board[8] << endl;
+            cout << "   |   |  " << endl;
 
+            cout << endl;
+
+        }
+
+        void updateBoard(int move, const playerOptions &playerTurn) {
+            if (playerTurn.turn == playerTurn.firstPlayer)
+                game_board[move] = "X";
+            else
+                game_board[move] = "O";
+            newBoard();
+        }
+}; 
 
 
 
@@ -95,6 +95,7 @@ int main() {
         cout << "Enter a valid game option\n";
         cout << "or choose player to start a game : ";
         cin >> userInput;
+        board.updateBoard(userInput, player);
         
         if(toupper(userInput) == 'Q') {
             // exit the application
